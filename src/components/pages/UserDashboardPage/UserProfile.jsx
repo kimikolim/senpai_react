6dd1cd9f59ad3ba2d2676e42d939196bfc9d99a4
Qiyang <qiyang.xu@oracle.com>
2021-07-24 20:36:48 +0800

    Profile State changes

--- src/components/pages/UserDashboardPage/UserProfile.jsx
+++ src/components/pages/UserDashboardPage/UserProfile.jsx
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
 import './UserProfile.scss';
-import { RangeStepInput } from 'react-range-step-input';
-
-const forceNumber = function (n) {
-	n = Number(n);
-	if (isNaN(n) || typeof n === 'undefined') {
-		n = 0;
-	}
-	return n;
-};
+import CategoryForm from './components/categoryForm';
+import AddButton from './components/addButton';
+import { SubmitButton } from './components/submitButton';
+import { EditButton } from './components/editButton';
 
 export class UserProfile extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			value: 30,
+			counter: 0,
+			forms: [{ id: Math.random(), data: {} }],
 		};
+		this.handleAddClick = this.handleAddClick.bind(this);
+		this.handleDeleteClick = this.handleDeleteClick.bind(this);
 	}
+	handleAddClick() {
+		this.setState((prevState) => {
+			let newForm = { id: Math.random(), data: {} };
+			return { forms: [...prevState.forms, newForm] };
+		});
+	}
+
+	handleDeleteClick(id) {
+		this.setState((prevState) => {
+			let updatedForms = prevState.forms.filter((form) => form.id !== id);
+			return { forms: updatedForms };
+		});
+	}
+
 	render() {
+		let { counter } = this.state;
+		let formComponents = this.state.forms.map((form) => {
+			return <CategoryForm key={form.id} delete={this.handleDeleteClick} id={form.id} />;
+		});
+		const buttonAdd = () => {
+			if (counter) {
+				return <CategoryForm />;
+			}
+		};
+
 		return (
 			<div className="container">
 				<div className="columns">
@@ -32,100 +54,21 @@ export class UserProfile extends Component {
 						<h1>
 							<strong>Skills</strong>
 						</h1>
-						<div className="columns">
-							<div className="category column">
-								<div className="select is-primary">
-									<select>
-										<option>Select Category</option>
-										<option>Academia</option>
-										<option>Fitness</option>
-										<option>Cooking & Baking</option>
-										<option>Arts & Design</option>
-									</select>
-								</div>
-
-								<div className="skill">
-									<div className="sub-skill">
-										<div className="select">
-											<select>
-												<option>Select Sub-Category</option>
-												<option>Physics</option>
-												<option>Others</option>
-											</select>
-										</div>
-										<div className="input-bar">
-											<input
-												class="input"
-												type="text"
-												placeholder="Specify your skill!"
-											></input>
-										</div>
-									</div>
-									<div className="slidey-info">
-										<div class="control">
-											<div class="select">
-												<select>
-													<option>Years of Experience</option>
-													<option>1</option>
-													<option>2</option>
-													<option>3</option>
-													<option>4</option>
-													<option>5 or more</option>
-												</select>
-											</div>
-										</div>
-										<div className="slider">
-											<h3>Your rate per hour ($)</h3>
-											<RangeStepInput
-												min={0}
-												max={100}
-												value={this.state.value}
-												step={1}
-												onChange={this.onChange.bind(this)}
-											/>
-											<div className="input-group-append">
-												<span className="input-group-text ml-2">
-													{this.state.value}
-												</span>
-											</div>
-										</div>
-										<div className="comments">
-											<input
-												class="input is-primary"
-												type="text"
-												placeholder="Additional Comments"
-											/>
-										</div>
-									</div>
-									<div className="my-buttons">
-										<button className="button is-primary is-small">
-											<i class="far fa-plus-square"></i>
-											<p>Add Skill</p>
-										</button>
-										<button className="button is-danger is-small">
-											<i class="far fa-minus-square"></i>
-											<p>Delete Skill</p>
-										</button>
-									</div>
-								</div>
-							</div>
-							<div className="my-buttons column is-1">
-								<button className="button is-primary is-small">
-									<i class="far fa-plus-square"></i>
-								</button>
-								<button className="button is-danger is-small">
-									<i class="far fa-minus-square"></i>
-								</button>
-							</div>
-						</div>
+						{formComponents}
 					</div>
+					<div className="my-buttons column is-1">
+						<AddButton handleAddClick={this.handleAddClick} />
+						<button className="button is-danger is-small">
+							<i class="far fa-minus-square"></i>
+						</button>
+					</div>
+				</div>
+				<div className="big-buttons">
+					<SubmitButton />
+					<EditButton />
 				</div>
 			</div>
 		);
-	}
-	onChange(e) {
-		const newVal = forceNumber(e.target.value);
-		this.setState({ value: newVal });
 	}
 }
 
---
+++ src/components/pages/UserDashboardPage/components/editButton.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class EditButton extends Component {
+	render() {
+		return (
+			<div className="edit-button">
+				<button className="button is-primary">
+					<p>Edit</p>
+				</button>
+			</div>
+		);
+	}
+}
